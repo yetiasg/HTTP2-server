aab2c40c7837fd0538fec050289bf578850361f8
cherryvue <yetiasg@gmail.com>
2021-11-07 10:08:04 +0100

    initial http2 setup

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,32 @@ import morgan from 'morgan'
 import cors from 'cors'
 import { config } from './config'
 
-// const server = http2.createSecureServer()
-
-const app = express()
-app.use(express.json())
-app.use(cors({origin: '*'}))
-app.use(morgan('dev'))
-
-app.get('/', (req:Req, res:Res, next:Next) => {
-  res.status(200).json({message: 'Root route'})
+const server = http2.createSecureServer({
+  "key": "",
+  "cert": ""
 })
 
-app.listen(config.server.PORT, () => {
+server.on('stream', (stream, headers) => {
+  stream.respond({
+    "content-type": "application/json",
+    "status": 200
+  })
+
+  stream.end(JSON.stringify({
+    "user": "Mati",
+    "id": 22
+  }))
+})
+
+// const app = express()
+// app.use(express.json())
+// app.use(cors({origin: '*'}))
+// app.use(morgan('dev'))
+
+// app.get('/', (req:Req, res:Res, next:Next) => {
+//   res.status(200).json({message: 'Root route'})
+// })
+
+server.listen(config.server.PORT, () => {
   console.log(`App listening on port ${config.server.PORT}`)
 })
